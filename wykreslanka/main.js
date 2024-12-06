fa4f64732ca4733e5a5b97f15561f1e307c77351
Fiolka1 <113060452+Fiolka1@users.noreply.github.com>
2024-12-06 19:40:45 +0100

    second commit and making apear some words

--- wykreslanka/main.js
+++ wykreslanka/main.js
@@ -3,19 +3,18 @@ function srakaa() {
   width = document.getElementById("width").value;
   table = document.getElementById("tablica");
   table_data_index = 0;
-  for (let index = 0; index < height+20; index++) {
+  for (let index = 0; index < height + 20; index++) {
     try {
       table.removeChild(table.rows[0]);
     } catch {
       console.log("niedziala");
     }
   }
-
   for (var index = 0; index < height; index++) {
     row = document.createElement("tr");
     row.id = "table_row_" + index;
     table.appendChild(row);
-    console.log(row);
+    // console.log(row);
 
     for (let jndex = 0; jndex < width; jndex++) {
       data = document.createElement("td");
@@ -25,7 +24,83 @@ function srakaa() {
         Math.floor(Math.random() * (90 - 65 + 1)) + 65
       );
       row.appendChild(data);
-      console.log(data);
+      // console.log(data);
     }
   }
+  ijndex=0;
+  splitted_word=[];
+  words.forEach(element => {
+    if (element>=width) {
+      splitted_word[ijndex] = element.split("");
+      // console.log(splitted_word)  
+      ijndex++;
+      
+    }
+  });
+  ijndex=0;
+  for (let index = 0; index < height; index++) {
+    // console.log(words[index])
+    if (splitted_word[index].length <= width) {
+      console.log(splitted_word[index])
+      switch (Math.floor(Math.random() * 2)) {
+        case 1:
+          
+          
+          console.log(splitted_word);
+          splitted_word[index].forEach((element) => {
+            // console.log(ijndex)
+            
+            document.getElementById("table_data_" + ijndex).innerHTML = element;
+            ijndex++;
+          });
+          break;
+        default:
+          break;
+      }
+    }
+    
+  }
+ 
 }
+words = [
+  "angular",
+  "be",
+  "cat",
+  "dog",
+  "elephant",
+  "fish",
+  "giraffe",
+  "house",
+  "ice",
+  "jungle",
+  "kite",
+  "lion",
+  "mountain",
+  "night",
+  "ocean",
+  "piano",
+  "queen",
+  "river",
+  "sun",
+  "tree",
+  "umbrella",
+  "vase",
+  "whale",
+  "xylophone",
+  "yellow",
+  "zebra",
+  "apple",
+  "banana",
+  "cherry",
+  "date",
+  "eagle",
+  "forest",
+  "grape",
+  "honey",
+  "island",
+  "jacket",
+  "kangaroo",
+  "lemon",
+  "monkey",
+  "nest",
+];
